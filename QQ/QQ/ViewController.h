9d8050fe7cb190da47c9baa9d4e0d2942bf3a800
古智勇 <jack-gu@172-10-31-211.lightspeed.rlghnc.sbcglobal.net>
2017-10-26 15:56:14 +0800

    creat

---
+++ QQ/QQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ
+//
+//  Created by JACK-GU on 2017/10/26.
+//  Copyright © 2017年 JACK-GU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QQ/QQ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QQ
+//
+//  Created by JACK-GU on 2017/10/26.
+//  Copyright © 2017年 JACK-GU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
